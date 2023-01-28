a81bf7f5347910e7af86bf988e9508d2d2ea924b
sales0108 <64178276+sales0108@users.noreply.github.com>
2023-01-27 23:53:52 -0300

    subindo terraform

---
+++ app/maint.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("chave.json")
+
+  project = "devopsteste-375800"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+resource "google_compute_firewall" "default" {
+  name    = "default-firewall"
+  network = "default"
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080"]
+  }
+
+}
+
+
+resource "google_compute_instance" "vmtestdevops" {
+  name         = "vmtestdevops"
+  machine_type = "e2-micro"
+  zone         = "us-central1-c"
+
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      }
+  }
+
+        metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq docker.io; sudo apt-get install -yq golang; sudo cd /; sudo git clone https://github.com/sales0108/devops_teste.git; sudo docker build -t my-golang-app /devops_teste/app/.; sudo docker run -itd -p 8080:8080 --name my-running-app my-golang-app; sudo docker exec -d my-running-app go run . "
+
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+}
